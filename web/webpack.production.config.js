bffa8f2217e2fbcd0a837b61828a2eeac89dce63
Jeff Powell <github@jeffpowell.dev>
2024-07-29 00:32:44 -0600

    setup production webpack config to extend the development config

--- web/webpack.production.config.js
+++ web/webpack.production.config.js
@@ -1,58 +1,22 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { optimization } = require('./webpack.development.config');
 
 module.exports = {
+  extends: path.resolve(__dirname, './webpack.development.config.js'),
   mode: 'production',
-    entry: {
-        login: './app/pages/login.js',
-        registerAdmin: './app/pages/registerAdmin.js',
-        lists: './app/pages/lists.js',
-        listCreate: './app/pages/listCreate.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist',
-        clean: true
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          exclude: /node_modules/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-          ],
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'styles.css',
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: 'app/**/*.html', to: '[name][ext]' },
-        ],
-      }),
-    ],
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true, // Removes console logs
-            },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // Removes console logs
           },
-        }),
-        new CssMinimizerPlugin(),
-      ],
-      splitChunks: {
-        chunks: 'all',
-      },
-    },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
   }
+}
