229cfe9ef93fa2307a1c2c5c4c41fbbc09a113c2
Jeff Powell <github@jeffpowell.dev>
2024-08-18 12:37:43 +0000

    Changing logout to be a span

--- web/app/navbar.js
+++ web/app/navbar.js
@@ -1,26 +1,21 @@
 require("./index")
 
 async function logout() {
-    try {
-        const response = await fetch("/auth", {
-            method: "DELETE",
-            // Set the FormData instance as the request body
-            headers: {
-                "Accept": "text/plain"
-            }
-        });
-        if (response.status >= 400) {
-            console.error("Unexpected error while logging out");
-        } else if (response.status === 200) {
-            window.location.href = "/auth";
-        }
-    } catch (e) {
-        console.error(e);
+    const response = await fetch("/auth", {
+        method: "DELETE",
+        headers: {
+            "Accept": "text/plain"
+        },
+    });
+    if (response.status === 204 || response.status === 200) {
+        window.location.href = response.headers.get("Location");
+    }  
+    else if (response.status >= 400) {
+        console.error("Unexpected error while logging out");
     }
 }
 
 const logouts = document.querySelectorAll(".logout");
 logouts.forEach(logoutEl => logoutEl.addEventListener("click", (event) => {
-    event.preventDefault();
     logout();
 }));
