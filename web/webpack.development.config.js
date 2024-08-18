896d1949be756d7220a771623309a8fffe9ebeae
Jeff Powell <github@jeffpowell.dev>
2024-08-18 04:38:40 -0600

    adding missing pages from webpack config

--- web/webpack.development.config.js
+++ web/webpack.development.config.js
@@ -4,13 +4,15 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        login: './app/pages/login.js',
-        registerAdmin: './app/pages/registerAdmin.js',
-        lists: './app/pages/lists.js',
-        listCreate: './app/pages/listCreate.js',
-        listEdit: './app/pages/listEdit.js',
-        listItems: './app/pages/listItems.js',
-        itemCreate: './app/pages/itemCreate.js',
+      itemCreate: './app/pages/itemCreate.js',
+      listCreate: './app/pages/listCreate.js',
+      listEdit: './app/pages/listEdit.js',
+      listItems: './app/pages/listItems.js',
+      lists: './app/pages/lists.js',
+      registerAdmin: './app/pages/registerAdmin.js',
+      login: './app/pages/login.js',
+      sharedList: './app/pages/sharedList.js',
+      sharedList404: './app/pages/sharedList404.js',
     },
     output: {
         filename: '[name].js',
