7366c760cc31bf59678e067300c49d4db5c01443
Jeff Powell <github@jeffpowell.dev>
2024-10-31 10:40:53 -0600

    adding cache-control headers to auth redirect

--- internal/handlers/middleware/requireAuth.go
+++ internal/handlers/middleware/requireAuth.go
@@ -15,12 +15,18 @@ func RequireAuth() Middleware {
 		return func(w http.ResponseWriter, r *http.Request) {
 
 			if !constants.ADMIN_EXISTS {
+				w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+				w.Header().Set("Pragma", "no-cache")
+				w.Header().Set("Expires", "0")
 				http.Redirect(w, r, "/admin/register", http.StatusSeeOther)
 				return
 			}
 			// Check if user has a valid session
 			session, _ := constants.COOKIE_STORE.Get(r, constants.COOKIE_NAME_SESSION)
 			if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+                w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+                w.Header().Set("Pragma", "no-cache")
+                w.Header().Set("Expires", "0")
 				http.Redirect(w, r, "/auth", http.StatusSeeOther)
 				return
 			}
