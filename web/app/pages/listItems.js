cf701ec2aa1f744f1f96212917b791a32517b25a
Jeff Powell <github@jeffpowell.dev>
2024-11-01 06:45:00 -0600

    partial #7 - ag grid is now rendering; backend call is mapped to display HTML for each field

--- web/app/pages/listItems.js
+++ web/app/pages/listItems.js
@@ -1,39 +1,52 @@
 require('../index')
 require('../navbar')
-import "@ag-grid-community/styles/ag-grid.min.css";
-import "@ag-grid-community/styles/ag-theme-quartz.min.css";
+import { themeQuartz } from '@ag-grid-community/theming';
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { createGrid } from "@ag-grid-community/core";
+
+const itemNameRenderer = function(params) {
+    if (params.data.url.Valid) {
+        return `<a href="${params.data.url.String}" class="underline" target="_blank" rel="nofollow noreferrer">${params.value}</a>`
+    } else {
+        return params.value;
+    }
+}
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const deleteItemButtons = document.querySelectorAll('.btn-delete-item');
     const grid = document.querySelectorAll('.item-grid');
 
     const gridOptions = {
+        theme: themeQuartz,
+        loadThemeGoogleFonts: true,
         rowData: [],
         columnDefs: [
             { 
                 field: "name", 
-                headerName: "Item"
-            },
-            { 
-                field: "url", 
-                headerName: "URL", 
-                valueGetter: p => p.data.Valid ? p.data.String : ""
+                headerName: "Item",
+                flex: 1,
+                cellRenderer: itemNameRenderer
             },
             { 
                 field: "priority", 
                 headerName: "Priority", 
                 type: 'numericColumn',
-                valueGetter: p => p.data.Valid ? p.data.Int64 : null
+                width: 125,
+                sort: 'asc',
+                valueGetter: p => p.data.priority.Valid ? p.data.priority.Int64 : null
             },
             { 
                 field: "notes", 
                 headerName: "Notes", 
-                valueGetter: p => p.data.Valid ? p.data.String : ""
+                flex: 2,
+                valueGetter: p => p.data.notes.Valid ? p.data.notes.String : ""
             },
         ],
-        getRowId: params => String(params.data.id)
+        domLayout: 'autoHeight',
+        getRowId: params => String(params.data.id),
+        autoSizeStrategy: {
+            type: "fitGridWidth",
+        }
     };
     grid.forEach(g => {
         const gridApi = createGrid(g, gridOptions, {
