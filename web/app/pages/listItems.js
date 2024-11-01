d70ae14f8dd3f2af81672bd23d43f1fb510204bc
Jeff Powell <github@jeffpowell.dev>
2024-11-01 14:28:52 -0600

    partial #7 - Added actions column to items ag grid

--- web/app/pages/listItems.js
+++ web/app/pages/listItems.js
@@ -12,8 +12,63 @@ const itemNameRenderer = function(params) {
     }
 }
 
+class ActionButtonsComponent {
+    eGui;
+    deleteButton;
+    eventListener;
+
+    init(params) {
+        this.eGui = document.createElement('div');
+        this.eGui.classList.add('flex');
+        this.eGui.innerHTML = 
+            "<a href='/list/"+params.context.listId+"/item/"+params.data.id+"/edit'>"
+            // + "<!-- https://heroicons.com/ pencil -->"
+            +     "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6 text-gray-600'>"
+            +         "<path stroke-linecap='round' stroke-linejoin='round' d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125' />"
+            +     "</svg>"
+            + "</a>";
+        this.deleteButton = document.createElement('button');
+        this.deleteButton.setAttribute("type", "button");
+        this.deleteButton.dataset.listId = params.context.listId;
+        this.deleteButton.dataset.itemId = params.data.id;
+        this.deleteButton.innerHTML = 
+            // +  <!-- https://heroicons.com/ trash -->"
+            "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='size-6 text-red-900'>"
+            +     "<path stroke-linecap='round' stroke-linejoin='round' d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0' />"
+            + "</svg>"
+        this.eventListener = async event => {
+            let listId = this.deleteButton.dataset.listId;
+            let itemId = this.deleteButton.dataset.itemId;
+            const response = await fetch('/list/' + listId + '/item/' + itemId, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            if (response.status === 204 || response.status === 200) {
+                window.location.href = '/list/' + listId;
+            }
+        }
+        this.deleteButton.addEventListener('click', this.eventListener);
+        this.eGui.appendChild(this.deleteButton);
+    }
+
+    getGui() {
+        return this.eGui;
+    }
+
+    refresh() {
+        return true;
+    }
+
+    destroy() {
+        if (this.deleteButton) {
+            this.deleteButton.removeEventListener('click', this.eventListener);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    const deleteItemButtons = document.querySelectorAll('.btn-delete-item');
     const grid = document.querySelectorAll('.item-grid');
 
     const gridOptions = {
@@ -41,6 +96,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 flex: 2,
                 valueGetter: p => p.data.notes.Valid ? p.data.notes.String : ""
             },
+            {
+                field: "actions",
+                headerName: "Actions",
+                width: 100,
+                cellRenderer: ActionButtonsComponent
+            }
         ],
         domLayout: 'autoHeight',
         getRowId: params => String(params.data.id),
@@ -49,31 +110,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     };
     grid.forEach(g => {
-        const gridApi = createGrid(g, gridOptions, {
+        let listId = g.dataset.listId;
+        let augmentedGridOptions = gridOptions;
+        augmentedGridOptions.context = {listId: listId};
+        const gridApi = createGrid(g, augmentedGridOptions, {
             modules: [
                 ClientSideRowModelModule,
             ]
         });
-        let listId = g.dataset.listId;
         fetch("/list/"+listId+"/items")
             .then(response => response.json())
             .then(data => gridApi.setGridOption("rowData", data));
     });
-
-    deleteItemButtons.forEach(deleteItem => {
-        deleteItem.addEventListener('click', async (event) => {
-            let listId = deleteItem.dataset.listId;
-            let itemId = deleteItem.dataset.itemId;
-            const response = await fetch('/list/' + listId + '/item/' + itemId, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-            if (response.status === 204 || response.status === 200) {
-                window.location.href = '/list/' + listId;
-            }
-        });
-    });
-
 });
