9ddf4573f8eda61502926046e886c079a5783ff9
Jeff Powell <github@jeffpowell.dev>
2024-11-17 08:09:49 -0700

    fixes #18 and partial #17 - switching 'CREATE INDEX' statements to check IF EXISTS; adding new columns to introduce concept of groups

--- internal/database/init.sql
+++ internal/database/init.sql
@@ -1,29 +1,70 @@
+----------------------------------------------------
+--          listaway.list table
+----------------------------------------------------
 CREATE TABLE IF NOT EXISTS listaway.list (
-    Id SERIAL PRIMARY KEY,
-    UserId BIGINT NOT NULL,
-    Name VARCHAR NOT NULL,
-    Description VARCHAR NULL,
-    ShareCode VARCHAR
+    id SERIAL PRIMARY KEY,
+    userid BIGINT NOT NULL,
+    name VARCHAR NOT NULL,
+    description VARCHAR NULL,
+    sharecode VARCHAR
 );
 
-CREATE INDEX ON listaway.list (UserId);
-CREATE INDEX ON listaway.list (ShareCode);
+CREATE INDEX IF NOT EXISTS list_userid_idx ON listaway.list (userid);
+CREATE INDEX IF NOT EXISTS list_sharecode_idx ON listaway.list (sharecode);
 
+----------------------------------------------------
+--          listaway.item table
+----------------------------------------------------
 CREATE TABLE IF NOT EXISTS listaway.item (
-    Id SERIAL PRIMARY KEY,
-    ListId BIGINT NOT NULL,
-    Name VARCHAR NOT NULL,
-    URL VARCHAR,
-    Notes VARCHAR,
-    Priority INT
+    id SERIAL PRIMARY KEY,
+    listid BIGINT NOT NULL,
+    name VARCHAR NOT NULL,
+    url VARCHAR,
+    notes VARCHAR,
+    priority INT
 );
 
-CREATE INDEX ON listaway.item (ListId);
+CREATE INDEX IF NOT EXISTS item_listid_idx ON listaway.item (listid);
 
+----------------------------------------------------
+--          listaway.user table
+----------------------------------------------------
 CREATE TABLE IF NOT EXISTS listaway.user (
-    Id SERIAL,
-    Email VARCHAR PRIMARY KEY,
-    Name VARCHAR,
-    PasswordHash VARCHAR NOT NULL,
-    Admin BOOLEAN NOT NULL
+    id SERIAL,
+    email VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    passwordhash VARCHAR NOT NULL,
+    admin BOOLEAN NOT NULL,
+    instanceAdmin BOOLEAN NOT NULL
 );
+
+-- Migration from 1.6.0 to 1.7.0
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_schema = 'listaway'
+        AND table_name = 'user'
+        AND column_name = 'instanceadmin'
+    ) THEN
+        ALTER TABLE listaway.user
+        ADD COLUMN InstanceAdmin BOOLEAN DEFAULT false NOT NULL;
+    END IF;
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_schema = 'listaway'
+        AND table_name = 'user'
+        AND column_name = 'groupid'
+    ) THEN
+        ALTER TABLE listaway.user
+        ADD COLUMN groupid INTEGER DEFAULT 0 NOT NULL; --temporary default to get the column created
+        
+        ALTER TABLE listaway.user
+        ALTER COLUMN groupid DROP DEFAULT;
+    END IF;
+    
+END $$;
+
+CREATE INDEX IF NOT EXISTS user_groupid_idx ON listaway.user (groupid)
